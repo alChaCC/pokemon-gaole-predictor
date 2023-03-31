7c79d53cd6624dc105b4325d4fa3dfeaf49904e6
Aloha Chen <y.alohac@gmail.com>
2023-03-31 16:01:19 +0800

    loop & match

--- src/server.rs
+++ src/server.rs
@@ -5,6 +5,9 @@
 
 // by default every thing in Rust is private
 // pub keyword makes the struct public
+
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -25,5 +28,39 @@ impl Server {
   // if we want to allow the method to be called on an immutable or mutable instance, we can use &self or &mut self
   pub fn run(self) {
       println!("Listening on {}", self.addr);
+
+      // this is recoverable error, but we don't want to recover from it so we use unwrap.
+      // so if the result is Err, the program will terminate.
+      // if we want to handle the error, we can use match
+      let listener = TcpListener::bind(&self.addr).unwrap();
+
+      // infinite loop
+      // no need to use while true
+      // loop can be labeled !!!
+      // syntax: 'label: loop { ... }
+      // ex:
+      // 'outer: loop {
+      //    loop {
+      //      break 'outer;
+      //    }
+      // }
+      loop {
+          // match is an expression that returns a value
+          // the code will not compile if we don't handle ALL the possible cases
+          // match can also match against ranges of values
+          // ex:
+          // match number {
+          //   1 => println!("one"),
+          //   2 | 3 | 5 | 7 | 11 => println!("this is a prime"),
+          //   13..=19 => println!("a teen"),
+          //   _ => println!("not interesting"),
+          // }
+          match listener.accept() {
+              Ok((stream, _)) => {
+                println!("Accepted a connection");
+              }
+              Err(e) => println!("Failed to establish a connection: {}", e),
+          }
+      }
   }
 }
