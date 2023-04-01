cb03bdaf1bd67cc22663c98cf3c5905d122b487b
Aloha Chen <y.alohac@gmail.com>
2023-04-01 08:31:54 +0800

    1. parse() => impl FromStr 2. parse()? => impl From for the Error 3. patten match for better function handling

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 // can give a number to each variant
 // if POST = 5, then PUT = 6, etc.
 // enum is a type that can be one of a few different variants, like a union in C
@@ -12,3 +14,24 @@ pub enum Method {
   TRACE,
   PATCH,
 }
+
+impl FromStr for Method {
+  type Err = MethodError;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s {
+      "GET" => Ok(Self::GET),
+      "POST" => Ok(Self::POST),
+      "PUT" => Ok(Self::PUT),
+      "DELETE" => Ok(Self::DELETE),
+      "HEAD" => Ok(Self::HEAD),
+      "CONNECT" => Ok(Self::CONNECT),
+      "OPTIONS" => Ok(Self::OPTIONS),
+      "TRACE" => Ok(Self::TRACE),
+      "PATCH" => Ok(Self::PATCH),
+      _ => Err(MethodError),
+    }
+  }
+}
+
+pub struct MethodError;
--- src/http/request.rs
+++ src/http/request.rs
@@ -9,7 +9,7 @@ BODY
 // Some is a tuple struct that wraps a String
 // None is a unit struct that doesn't hold any value
 // Option<T> will automatically be imported into scope when we use it
-use super::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Display, Result as FmtResult, Formatter, Debug};
@@ -75,11 +75,38 @@ impl TryFrom<&[u8]> for Request {
 
     // way 2
     let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-    let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+    let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
     let (protocal, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
     if protocal != "HTTP/1.1" {
       return Err(ParseError::InvalidProtocol);
+    }
+
+    // because we implement FromStr for Method, so we can use parse() to convert a string
+    // and because we implement From<Method> for ParseError, so we can use parse()? to convert a string
+    let method: Method = method.parse()?;
+    let mut query_string = None;
+    // way 1
+    // match path.find('?') {
+    //   Some(i) => {
+    //     query_string = Some(&path[i + 1..]);
+    //     path = &path[..i];
+    //   },
+    //   None => {},
+    // }
+
+    // way 2
+    // let q = path.find('?');
+    // if q.is_some() {
+    //   let i = q.unwrap();
+    //   query_string = Some(&path[i + 1..]);
+    //   path = &path[..i];
+    // }
+
+    // way 3 (common pattern in Rust), better solution
+    if let Some(i) = path.find('?') {
+      query_string = Some(&path[i + 1..]);
+      path = &path[..i];
     }
 
     unimplemented!()
@@ -96,6 +123,12 @@ pub enum ParseError {
 impl From<Utf8Error> for ParseError {
   fn from(_: Utf8Error) -> Self {
     Self::InvalidEncoding
+  }
+}
+
+impl From<MethodError> for ParseError {
+  fn from(_: MethodError) -> Self {
+    Self::InvalidMethod
   }
 }
 
