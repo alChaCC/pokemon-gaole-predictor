fba4d420dc3cfe15db329215b2ff55c180721a49
Aloha Chen <y.alohac@gmail.com>
2023-04-02 09:22:58 +0800

    1. support file securily

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,4 +1,5 @@
 use super::server::Handler;
+use std::fs;
 pub struct WebsiteHandler {
     public_path: String,
 }
@@ -9,11 +10,21 @@ impl WebsiteHandler {
     }
 
     fn read_file(&self, file_path: &str) -> Option<String> {
-        let file_path = format!("{}/{}", self.public_path, file_path);
+        let path = format!("{}/{}", self.public_path, file_path);
         // ok() is a handy method that converts a Result to an Option
         // if the Result is Ok, it will return Some
         // if the Result is Err, it will return None
-        std::fs::read_to_string(file_path).ok()
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory traversal attack detected: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
     }
 }
 
@@ -51,10 +62,20 @@ impl Handler for WebsiteHandler {
                     crate::http::StatusCode::Ok200,
                     Some("<h1>Hello</h1>".to_string()),
                 ),
-                _ => crate::http::Response::new(
-                    crate::http::StatusCode::NotFound404,
-                    Some("<h1>Not Found</h1>".to_string()),
-                ),
+                // we can match path and pass the path to read_file
+                // but here has security issue
+                // if we pass ../../etc/passwd, it will read the file
+                // so we need to check if the path is valid
+                path => match self.read_file(path) {
+                    Some(content) => crate::http::Response::new(
+                        crate::http::StatusCode::Ok200,
+                        Some(content),
+                    ),
+                    None => crate::http::Response::new(
+                        crate::http::StatusCode::NotFound404,
+                        Some("<h1>Not Found</h1>".to_string()),
+                    ),
+                },
             },
             _ => crate::http::Response::new(
                 crate::http::StatusCode::NotFound404,
