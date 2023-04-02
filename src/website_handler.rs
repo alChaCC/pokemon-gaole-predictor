a29657c8670f278ec3fe77838323af01a1ad2e26
Aloha Chen <y.alohac@gmail.com>
2023-04-02 08:42:29 +0800

    serving html files

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -7,6 +7,14 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let file_path = format!("{}/{}", self.public_path, file_path);
+        // ok() is a handy method that converts a Result to an Option
+        // if the Result is Ok, it will return Some
+        // if the Result is Err, it will return None
+        std::fs::read_to_string(file_path).ok()
+    }
 }
 
 impl Handler for WebsiteHandler {
@@ -37,7 +45,7 @@ impl Handler for WebsiteHandler {
             crate::http::Method::GET => match request.path() {
                 "/" => crate::http::Response::new(
                     crate::http::StatusCode::Ok200,
-                    Some("<h1>Welcome</h1>".to_string()),
+                    self.read_file("index.html"),
                 ),
                 "/hello" => crate::http::Response::new(
                     crate::http::StatusCode::Ok200,
